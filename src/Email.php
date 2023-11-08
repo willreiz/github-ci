4bb104a2390a080b9a8e1b219dec11ae767f656d
willreiz <149170188+willreiz@users.noreply.github.com>
2023-11-08 10:24:29 -0400

    Updated Email to IP

--- src/Email.php
+++ src/Email.php
@@ -27,7 +27,7 @@ final class Email
 
     private function ensureIsValidEmail(string $email): void
     {
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if (!filter_var($email, FILTER_VALIDATE_IP)) {
             throw new InvalidArgumentException(
                 sprintf(
                     '"%s" is not a valid email address',
